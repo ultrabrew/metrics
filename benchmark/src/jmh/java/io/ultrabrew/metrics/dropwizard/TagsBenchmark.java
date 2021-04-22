623afc8f4d457f859f23489c901389ca340c0b30
Smruti Ranjan Sahoo <smrutis@verizonmedia.com>
2021-04-21 21:05:09 -0700

    upgrade gradle and plugins

--- benchmark/src/jmh/java/io/ultrabrew/metrics/dropwizard/TagsBenchmark.java
+++ benchmark/src/jmh/java/io/ultrabrew/metrics/dropwizard/TagsBenchmark.java
@@ -57,7 +57,7 @@ public class TagsBenchmark {
     ultrabrewCounter = ultrabrewRegistry.counter("counter");
     ultrabrewTimer = ultrabrewRegistry.timer("timer");
     ultrabrewGauge = ultrabrewRegistry.gauge("gauge");
-    ultrabrewReporter = new SLF4JReporter("ultrabrew");
+    ultrabrewReporter = SLF4JReporter.builder().withName("ultrabrew").build();
     ultrabrewRegistry.addReporter(ultrabrewReporter);
 
     dropwizardRegistry = new com.codahale.metrics.MetricRegistry();
